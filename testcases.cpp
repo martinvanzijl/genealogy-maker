05bfc141e880cc1d732b2be795ce40dc83469f58
Martin van Zijl <martin.vanzijl@gmail.com>
2021-10-12 06:26:59 +1300

    Add test case for saving file.

--- testcases.cpp
+++ testcases.cpp
@@ -6,6 +6,10 @@
 #include <QDebug>
 #include <QObject>
 
+// =============================================================================
+// TestCaseHelper
+// =============================================================================
+
 class TestCaseHelper : public QObject
 {
     Q_OBJECT
@@ -14,7 +18,8 @@ public:
     virtual ~TestCaseHelper();
 
 private slots:
-    void handleDialog();
+    void handleOpenDialog();
+    void handleSaveDialog();
 };
 
 TestCaseHelper::~TestCaseHelper()
@@ -22,17 +27,34 @@ TestCaseHelper::~TestCaseHelper()
     // Avoid compiler error.
 }
 
-void TestCaseHelper::handleDialog()
+void TestCaseHelper::handleOpenDialog()
 {
     QWidget *activeModalWidget = QApplication::activeModalWidget();
     QFileDialog *dialog = dynamic_cast<QFileDialog *>(activeModalWidget);
 
     if (dialog)
     {
-        QTest::keyClicks(dialog, "van-zijl-new.xml");
+        dialog->selectFile("van-zijl-new.xml");
         QTimer::singleShot(0, dialog, SLOT(accept()));
     }
 }
+
+
+void TestCaseHelper::handleSaveDialog()
+{
+    QWidget *activeModalWidget = QApplication::activeModalWidget();
+    QFileDialog *dialog = dynamic_cast<QFileDialog *>(activeModalWidget);
+
+    if (dialog)
+    {
+        dialog->selectFile("saved-diagram.xml");
+        QTimer::singleShot(0, dialog, SLOT(accept()));
+    }
+}
+
+// =============================================================================
+// TestCases
+// =============================================================================
 
 class TestCases: public QObject
 {
@@ -44,6 +66,7 @@ public:
 private slots:
     void testGui();
     void testOpen();
+    void testSave();
 
 private:
     TestCaseHelper *m_helper;
@@ -82,10 +105,28 @@ void TestCases::testOpen()
     QTest::qWaitForWindowActive(&mainWindow);
 
     m_helper = new TestCaseHelper();
-    QTimer::singleShot(1000, m_helper, SLOT(handleDialog()));
+    QTimer::singleShot(1000, m_helper, SLOT(handleOpenDialog()));
     QTest::keyClicks(&mainWindow, "O", Qt::ControlModifier);
 
     QCOMPARE(mainWindow.windowTitle(), QString("van-zijl-new.xml - Genealogy Maker Qt"));
+}
+
+void TestCases::testSave()
+{
+    Q_INIT_RESOURCE(genealogymaker);
+
+    MainForm mainWindow;
+    mainWindow.moveToCenter();
+    mainWindow.show();
+    mainWindow.activateWindow();
+
+    QTest::qWaitForWindowActive(&mainWindow);
+
+    m_helper = new TestCaseHelper();
+    QTimer::singleShot(1000, m_helper, SLOT(handleSaveDialog()));
+    QTest::keyClicks(&mainWindow, "S", Qt::ControlModifier);
+
+    QCOMPARE(mainWindow.windowTitle(), QString("saved-diagram.xml - Genealogy Maker Qt"));
 }
 
 QTEST_MAIN(TestCases)
