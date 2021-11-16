a5878b5a87c730ec0dce9b3df3f212bfb174cc84
Martin van Zijl <martin.vanzijl@gmail.com>
2021-11-17 05:03:27 +1300

    Add deployment script for Linux.

---
+++ deploy-for-linux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# ----------------------------------------------------------------------
+# Script to make the Linux deployment.
+# ----------------------------------------------------------------------
+
+# Copy the executable.
+cp ~/Documents/build-genealogymaker-Desktop-Release/genealogymaker ~/AppDirs/genealogy-maker.AppDir/genealogy-maker/bin/genealogy-maker
+
+# Make the deployment directory.
+DEST_DIR="genealogy-maker-v8"
+mkdir $DEST_DIR
+
+# Go to the deployment directory.
+cd $DEST_DIR
+
+# Make the deployment.
+~/Downloads/build-linuxdeployqt-Desktop-Debug/bin/linuxdeployqt ~/AppDirs/genealogy-maker.AppDir/genealogy-maker/share/applications/genealogy-maker.desktop -verbose=2 -appimage -ignore-glob="/home/martin/*"
+
+# Copy the GEDCOM Python files.
+cp -R ../python-gedcom-1.0.0/ .
+
+# Go back.
+cd ..
